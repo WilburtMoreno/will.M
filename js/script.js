38c7895f8565a549369fc8e2e13a6572f6048b8d
Wilburt Moreno <wilburtmoreno1@gmail.com>
2020-09-24 20:27:00 -0700

    updated 9/24/2020
    prev and next button onload image

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ const lightbox = document.createElement('div');
 lightbox.id = 'lightbox';
 document.body.appendChild(lightbox);
 
-// gets all the images in a array form
+// gets all the images from DOM in a array form
 const images = document.querySelectorAll('.images');
 
 images.forEach( image => {
-
+    
     //EventListener
      image.addEventListener('click', e => {
         lightbox.classList.add('active');
@@ -18,9 +18,38 @@ images.forEach( image => {
             lightbox.removeChild(lightbox.firstChild);
         }
         lightbox.appendChild(img);
+
+        // Left Right Control
+        img.onload = function() {
+            const imgWidth = this.width;
+            const calcImgEdges = ((window.innerWidth - imgWidth) / 2) - 80;
+           
+            //TEST
+            console.log(calcImgEdges);
+
+            // Prev button
+            const prevBtn = document.createElement('a');
+            const prevName = document.createTextNode('prev');
+            prevBtn.appendChild(prevName);
+            lightbox.appendChild(prevBtn);
+            prevBtn.id = 'prev';
+            prevBtn.style.cssText = 'left: ' + calcImgEdges + 'px;';
+           
+
+            // Next button
+            const nextBtn = document.createElement('a');
+            const nextName = document.createTextNode('next');
+            nextBtn.appendChild(nextName);
+            lightbox.appendChild(nextBtn);
+            nextBtn.id = 'next';
+            nextBtn.style.cssText = 'right: ' + calcImgEdges + 'px;';
+        };
      });
+
+     
 });
 
+//EventListener
 lightbox.addEventListener('click', e => {
     if(e.target !== e.currentTarget) return
      lightbox.classList.remove('active');
