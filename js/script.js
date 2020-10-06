6c2b39b1ab2cf6fa45a1371498d5321d715722bf
Wilburt Moreno <wilburtmoreno1@gmail.com>
2020-10-05 20:32:14 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,17 +6,23 @@ document.body.appendChild(lightbox);
 // gets all the images from DOM in a array form
 const images = document.querySelectorAll('.images');
 
-images.forEach( image => {
-    
+const imgArr = [];
+let currentImg;
+
+images.forEach( (image, index) => {
+   // add the .images to an array called 'imgArr'
+   imgArr.push(image);
+
+   // adding id property to img attribute by its indexes plus one for non zero based index;
+   image.id = "image-"+ (index) ; 
+
     //EventListener
      image.addEventListener('click', e => {
         lightbox.classList.add('active');
         const img = document.createElement('img');
         img.src = image.src;
 
-        while(lightbox.firstChild) {
-            lightbox.removeChild(lightbox.firstChild);
-        }
+       closeImage(lightbox, index);
         lightbox.appendChild(img);
 
         // Left Right Control
@@ -25,7 +31,9 @@ images.forEach( image => {
             const calcImgEdges = ((window.innerWidth - imgWidth) / 2) - 80;
            
             //TEST
-            console.log(calcImgEdges);
+            //console.log(imgArr);
+             console.log(currentImg);
+             console.log('index: ' + index);
 
             // Prev button
             const prevBtn = document.createElement('a');
@@ -34,6 +42,7 @@ images.forEach( image => {
             lightbox.appendChild(prevBtn);
             prevBtn.id = 'prev';
             prevBtn.style.cssText = 'left: ' + calcImgEdges + 'px;';
+            prevBtn.setAttribute('onclick', 'changeImg(0)');
            
 
             // Next button
@@ -43,12 +52,42 @@ images.forEach( image => {
             lightbox.appendChild(nextBtn);
             nextBtn.id = 'next';
             nextBtn.style.cssText = 'right: ' + calcImgEdges + 'px;';
+            nextBtn.setAttribute('onclick', 'changeImg(1)');
         };
      });
 
      
 });
+// Method
+function closeImage(lightbox, index) {
+    while(lightbox.firstChild) {
+        lightbox.removeChild(lightbox.firstChild);
+    }
+    // resets the currentIndex to whatever index is clicked
+    currentImg = index;
+};
+function changeImg(prevNext) {
+    const zeroBasedDotLength = imgArr.length -1; 
+    let iterateImg;
+    console.log('----------------------------------------------------------------');
+    
+    if(prevNext === 0 ) {
+       iterateImg = imgArr[(currentImg-- -1)];
+        if(currentImg < 1) {
+            currentImg = zeroBasedDotLength;
+        }
+    }
+    if(prevNext === 1 ) {
+        iterateImg = imgArr[(currentImg++ -1)];
+        if(currentImg > zeroBasedDotLength) {
+            currentImg = 1;
+        } 
+    }
 
+    console.log('Object: ');
+    console.log(imgArr[currentImg]);
+    console.log('output: '+ currentImg);
+}
 //EventListener
 lightbox.addEventListener('click', e => {
     if(e.target !== e.currentTarget) return
